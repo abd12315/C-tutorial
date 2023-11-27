a2a9e5c3d065ec83fad50cb93aed1fba7064b4f3
abd12315 <67475230+abd12315@users.noreply.github.com>
2023-11-27 21:20:56 +0000

    my code :)

---
+++ tutorials/augementedassignmentop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    // this one explains the working of augemented assignmented operator in detail
+    /*the = use to replace operator where operator takes the value of the arguments and return the value back as 
+    the same variable X=X+1
+    X+=1*/
+    int x=10;
+    //x=x+2;
+    //x+=2;
+    //x=x-3;
+    // x-=3;
+    // x=x*3;
+    //x*=3;
+    //x=x/3;
+    //x/=3;
+
+    // augemented assignment operator is same as the each operation describe individually of rach individual augemented operations//
+
+    //let's test this theory out IRL?
+    printf("Value:%d",x);
+    return 0;
+}
